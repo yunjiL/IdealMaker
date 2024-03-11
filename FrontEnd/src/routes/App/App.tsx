120aca2d1bbfced7fa99fdacf400910823784c22
정승원 <seungwon1213@naver.com>
2024-03-11 12:50:35 +0900

    Design: Main page format design
    메인 페이지, 스크롤 디자인 및 라우팅 구성
    
    Related to: #174, 184

---
+++ FrontEnd/src/routes/App/App.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import {Route, Routes} from "react-router-dom";
+import Main from "../Main/index.tsx";
+
+
+const Container = styled.div`
+  width: 40%;
+  height: 100%;
+  background-color: floralwhite; 
+
+  /* 모바일 환경에서는 너비를 100%로 */
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+function App() {
+  return (
+    <>
+        <Container>
+            <Routes>
+                <Route path="/*" element={<Main/>}/>
+            </Routes>
+        </Container>
+    </>
+  )
+}
+
+export default App
---
+++ FrontEnd/src/routes/Main/styles.tsx
@@ -0,0 +1,14 @@
+import styled from "tailwind-styled-components";
+
+export const MainLayout = styled.div`
+    flex-col
+    w-[100%]
+    h-[100%]
+    bg-white
+`
+
+export const BodySection=styled.section`
+    h-[100%] 
+    max-h-[100%]
+    overflow-y-auto
+`
