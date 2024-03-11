d7660fde59c6bc1146a9ccd299f4862042473648
floriferre <jun1778@naver.com>
2024-03-11 12:42:31 +0900

    chore: spring boot initial setting
    Related to: #S10P22A302-128

---
+++ BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/IdealMakerApplication.java
@@ -0,0 +1,13 @@
+package com.ideal.idealmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdealMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdealMakerApplication.class, args);
+	}
+
+}
---
+++ BackEnd/IdealMaker/src/test/java/com/ideal/idealmaker/IdealMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideal.idealmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdealMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
