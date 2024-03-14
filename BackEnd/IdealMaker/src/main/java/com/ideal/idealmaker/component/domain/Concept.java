050fecd2290687ee01cc39611146d36af83a8bbe
Yunji Lee <luj2868@gmail.com>
2024-03-14 11:15:52 +0900

    feat: concept, hairstyle 필드 추가
    gender 필드 추가
    - 연관관계 생성

--- BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/component/domain/Concept.java
+++ BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/component/domain/Concept.java
@@ -4,9 +4,12 @@ package com.ideal.idealmaker.component.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,6 +23,10 @@ public class Concept {
 	@Column(name = "concept_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "gender_id")
+	private Gender gender;
 
 	@Column(nullable = false)
 	private String concept;
--- BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/component/domain/HairStyle.java
+++ BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/component/domain/HairStyle.java
@@ -2,9 +2,12 @@ package com.ideal.idealmaker.component.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +21,10 @@ public class HairStyle {
 	@Column(name = "hair_style_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "gender_id")
+	private Gender gender;
 
 	@Column(nullable = false)
 	private String hairStyle;
