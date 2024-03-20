04fc769fa4476ace1074699d23add3572edb88dd
Yunji Lee <luj2868@gmail.com>
2024-03-20 17:29:11 +0900

    feat: 기능 별로 dto 분리

--- BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/survey/dto/SurveyConceptListDto.java
+++ BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/survey/dto/SurveyConceptListDto.java
@@ -3,6 +3,8 @@ package com.ideal.idealmaker.survey.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.ideal.idealmaker.component.domain.EyeStyle;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,43 +21,18 @@ public class SurveyConceptListDto {
 	private String genderNm;
 
 	@Builder.Default
-	private List<age> ageList = new ArrayList<>();
+	private List<AgeDto> ageList = new ArrayList<>();
 
 	@Builder.Default
-	private List<face> faceList = new ArrayList<>();
+	private List<FaceDto> faceList = new ArrayList<>();
 
 	@Builder.Default
-	private List<skinColor> skinColorList = new ArrayList<>();
+	private List<SkinColorDto> skinColorList = new ArrayList<>();
 
 	@Builder.Default
-	private List<eyeStyle> eyeStyleList = new ArrayList<>();
+	private List<EyeStyleDto> eyeStyleList = new ArrayList<>();
 
 	@Builder.Default
-	private List<concept> conceptList = new ArrayList<>();
-
-	private class age {
-		Integer ageId;
-		String age;
-	}
-
-	private class face {
-		Integer faceId;
-		String face;
-	}
-
-	private class skinColor {
-		Integer skinColorId;
-		String skinColor;
-	}
-
-	private class eyeStyle {
-		Integer eyeStyleId;
-		String eyeStyle;
-	}
-
-	private class concept {
-		Integer conceptId;
-		String concept;
-	}
-
+	private List<ConceptDto> conceptList = new ArrayList<>();
+	
 }
--- BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/survey/dto/SurveyCustomListDto.java
+++ BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/survey/dto/SurveyCustomListDto.java
@@ -3,9 +3,10 @@ package com.ideal.idealmaker.survey.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.ideal.idealmaker.component.domain.HairLength;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
@@ -16,108 +17,47 @@ public class SurveyCustomListDto {
 	@NoArgsConstructor
 	@AllArgsConstructor
 	@Builder
-	public static class InnerParent{
+	public static class Custom {
 		private String type;
 
 		private String genderNm;
 
 		@Builder.Default
-		private List<age> ageList = new ArrayList<>();
+		private List<AgeDto> ageList = new ArrayList<>();
 
 		@Builder.Default
-		private List<face> faceList = new ArrayList<>();
+		private List<FaceDto> faceList = new ArrayList<>();
 
 		@Builder.Default
-		private List<skinColor> skinColorList = new ArrayList<>();
+		private List<SkinColorDto> skinColorList = new ArrayList<>();
 
 		@Builder.Default
-		private List<eyeStyle> eyeStyleList = new ArrayList<>();
+		private List<EyeStyleDto> eyeStyleList = new ArrayList<>();
+
+		private String eyeColor;
 
 		@Builder.Default
-		private List<background> backgroundList = new ArrayList<>();
+		private List<BackgroundDto> backgroundList = new ArrayList<>();
 
 		@Builder.Default
-		private List<clothStyle> clothStyleList = new ArrayList<>();
+		private List<ClothStyleDto> clothStyleList = new ArrayList<>();
 
 		@Builder.Default
-		private List<hairStyle> hairStyleList = new ArrayList<>();
+		private List<HairStyleDto> hairStyleList = new ArrayList<>();
+
+		private String hairColor;
 	}
 
 	@Getter
 	@SuperBuilder
 	@AllArgsConstructor
 	@NoArgsConstructor
-	public static class FemaleCustom extends InnerParent {
+	public static class FemaleCustom extends Custom {
 
-		private List<hairLength> hairLengthList;
+		private List<HairLengthDto> hairLengthList;
 
 		@Builder.Default
-		private List<makeUp> makeUpList = new ArrayList<>();
-
-		FemaleCustom(String type, String genderNm, List<age> ageList, List<face> faceList,
-			List<skinColor> skinColorList,
-			List<eyeStyle> eyeStyleList, List<background> backgroundList, List<clothStyle> clothStyleList,
-			List<hairStyle> hairStyleList) {
-			super(type, genderNm, ageList, faceList, skinColorList, eyeStyleList, backgroundList, clothStyleList,
-				hairStyleList);
-		}
-	}
-
-	@Data
-	@Builder
-	public static class maleCustom extends InnerParent {
-
-
-	}
-
-	private class age {
-		Integer ageId;
-		String age;
-	}
-
-	private class face {
-		Integer faceId;
-		String face;
-	}
-
-	private class skinColor {
-		Integer skinColorId;
-		String skinColor;
-	}
-
-	private class eyeStyle {
-		Integer eyeStyleId;
-		String eyeStyle;
-	}
-
-	private class concept {
-		Integer conceptId;
-		String concept;
-	}
-
-	private class background {
-		Integer backgroundId;
-		String background;
-	}
-
-	private class clothStyle {
-		Integer clothStyleId;
-		String clothStyle;
-	}
-
-	private class hairStyle {
-		Integer hairStyleId;
-		String hairStyle;
-	}
-
-	private class hairLength {
-		Integer hairLengthId;
-		String hairLength;
-	}
-
-	private class makeUp {
-		Integer makeUpId;
-		String makeUp;
+		private List<MakeUpDto> makeUpList = new ArrayList<>();
 	}
 
 }
