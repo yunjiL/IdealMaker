da9a45d927d29262f43dd66ea4e01f7d39d0c1e8
Lim Kim <jin_in_1992@naver.com>
2024-03-21 15:21:55 +0900

    feat: repository에 getById(Integer id) 추가
    - getById 추가

--- BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/survey/repository/ClothStyleRepository.java
+++ BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/survey/repository/ClothStyleRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ideal.idealmaker.component.domain.ClothStyle;
 
 public interface ClothStyleRepository extends JpaRepository<ClothStyle, Integer> {
+	ClothStyle getById(Integer id);
 }
