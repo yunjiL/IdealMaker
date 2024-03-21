2bb8c711ea67cbf7977bf13131d2d1c0e0da9d31
이지은 <ju3391@naver.com>
2024-03-21 09:52:48 +0900

    fix : Gallery 기능 구현

--- BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/gallery/GalleryApplication.java
+++ BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/gallery/GalleryApplication.java
@@ -3,7 +3,8 @@ package com.ideal.idealmaker.gallery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ideal.idealmaker.gallery")
+
 public class GalleryApplication {
 
     public static void main(String[] args) {
