03fd8878da666ec9af278654793a8282bf49c021
floriferre <jun1778@naver.com>
2024-03-21 10:59:09 +0900

    feat: 접근 권한 추가 패키지_JYM
    Related to: S10P22A302-211

---
+++ BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.ideal.idealmaker.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173", "http://j10a302.p.ssafy.io", "http://j10a302.p.ssafy.io:80", "https://j10a302.p.ssafy.io:443")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS");
+    }
+}
