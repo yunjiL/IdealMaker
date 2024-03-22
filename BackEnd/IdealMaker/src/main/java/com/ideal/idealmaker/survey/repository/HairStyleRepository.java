136a6080bcee837a03ef0e6bd77d31ab34a7cd36
Yunji Lee <luj2868@gmail.com>
2024-03-22 16:12:01 +0900

    fix: repository 생성

--- BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/survey/repository/HairStyleRepository.java
+++ BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/survey/repository/HairStyleRepository.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ideal.idealmaker.component.domain.HairStyle;
 import com.ideal.idealmaker.survey.dto.SelectDto;
 
 public interface HairStyleRepository extends JpaRepository<HairStyle, Integer> {
 
-	@Query("SELECT new com.ideal.idealmaker.survey.dto.SelectDto(h.id, h.hairStyle) FROM HairStyle h")
-	List<SelectDto> findIdAndHairStyle();
+	@Query("SELECT new com.ideal.idealmaker.survey.dto.SelectDto(h.id, h.hairStyle) "
+		   + "FROM HairStyle h WHERE h.gender.id = :genderId")
+	List<SelectDto> findIdAndHairStyle(@Param("genderId") int genderId);
 }
