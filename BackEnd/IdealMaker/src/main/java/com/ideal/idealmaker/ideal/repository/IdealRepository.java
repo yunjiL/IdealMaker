b7e1422152b3e865c9ce35297330592781a4e031
Lim Kim <jin_in_1992@naver.com>
2024-03-27 13:29:57 +0900

    feat: idealRepository 생성

---
+++ BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/ideal/repository/IdealRepository.java
@@ -0,0 +1,8 @@
+package com.ideal.idealmaker.ideal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ideal.idealmaker.ideal.domain.Ideal;
+
+public interface IdealRepository extends JpaRepository<Ideal,Long> {
+}
