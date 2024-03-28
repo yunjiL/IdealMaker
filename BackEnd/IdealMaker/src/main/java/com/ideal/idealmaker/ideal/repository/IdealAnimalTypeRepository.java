23b1c1b3da19be045d2e9d0cd8b9e2995b048d8e
Lim Kim <jin_in_1992@naver.com>
2024-03-28 15:26:58 +0900

    fix: erd 수정하면서 IdealAnimalType과 관련된 도메인 삭제

--- BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/ideal/repository/IdealAnimalTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ideal.idealmaker.ideal.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ideal.idealmaker.ideal.domain.IdealAnimalType;
-
-public interface IdealAnimalTypeRepository extends JpaRepository<IdealAnimalType,Long> {
-}
