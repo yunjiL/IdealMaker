fca486bf5b79d433e2d817809848c9f6354fd991
이지은 <ju3391@naver.com>
2024-04-01 11:00:55 +0900

    feat : worldcup 기능 구현

--- BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/gallery/service/ImageService.java
+++ BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/gallery/service/ImageService.java
@@ -5,8 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ImageService {
+
     Page<ImageDTO> findAllImages(Pageable pageable);
     ImageDTO findImageById(Long idealId);
     Page<ImageDTO> findByAnimalType(String animalType, Pageable pageable);
     Page<ImageDTO> findByGenderId(Integer genderId, Pageable pageable);
+
 }
---
+++ BackEnd/IdealMaker/src/main/java/com/ideal/idealmaker/worldcup/dto/WorldcupDTO.java
@@ -0,0 +1,15 @@
+package com.ideal.idealmaker.worldcup.dto;
+
+import lombok.*;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class WorldcupDTO {
+    private Long idealId;
+    private String idealURL;
+    private String animalType; //동물상
+    private Integer genderId;  //성별 id
+}
